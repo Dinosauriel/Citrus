62b4164bd04c6ed1c8b46e92a6fd6ffa0fbe828c
Aurel Feer <aurel.feer@gmail.com>
2022-03-10 21:02:09 +0100

    moved some buffers to heap allocation

--- src/main.rs
+++ src/main.rs
@@ -222,8 +222,11 @@ fn main() {
         let mut input_state: InputState = Default::default();
 
         // +++++++++++++++
-        const MAP_SIZE: usize = 64;
-        let mut index_buffer_data = [0u32; 6 * (MAP_SIZE - 1) * (MAP_SIZE - 1)];
+        const MAP_SIZE: usize = 512;
+        // let mut index_buffer_data = std::boxed::Box::new([0u32; 6 * (MAP_SIZE - 1) * (MAP_SIZE - 1)]);
+        let mut index_buffer_data: Vec<u32> = Vec::with_capacity(6 * (MAP_SIZE - 1) * (MAP_SIZE - 1));
+        index_buffer_data.resize(6 * (MAP_SIZE - 1) * (MAP_SIZE - 1), 0);
+        println!("index buffer size: {} integers, {} bytes", index_buffer_data.capacity(), index_buffer_data.capacity() * 4);
         for i in 0 .. MAP_SIZE - 1 {
             for j in 0 .. MAP_SIZE - 1 {
                 let tl = y_x_to_i(i as u64, j as u64, MAP_SIZE as u64) as u32;
@@ -248,17 +251,18 @@ fn main() {
             vk::BufferUsageFlags::INDEX_BUFFER,
             vk::MemoryPropertyFlags::HOST_VISIBLE | vk::MemoryPropertyFlags::HOST_COHERENT);
 
-        fill_buffer(&base.device, index_buffer_memory, &index_buffer_memory_req, &index_buffer_data);
+        fill_buffer(&base.device, index_buffer_memory, &index_buffer_memory_req, index_buffer_data.as_ref());
         // +++++++++++++++
         let perlin = Perlin::new();
-        let mut vertices = [Vertex {
+        let mut vertices: Vec<Vertex> = Vec::with_capacity(MAP_SIZE * MAP_SIZE);
+        vertices.resize(MAP_SIZE * MAP_SIZE, Vertex{
             pos: [0.0, 0.0, 0.0, 0.0],
-            color: [0.0, 0.0, 0.0, 0.0],
-        }; MAP_SIZE * MAP_SIZE];
+            color: [0.0, 1.0, 0.0, 1.0]
+        });
         for i in 0 .. MAP_SIZE {
             for j in 0 .. MAP_SIZE {
                 let y = 10. * perlin.get([(i as f64) / 100. + 0.1, (j as f64) / 100. + 0.1]);
-                println!("{:?}", y);
+                // println!("{:?}", y);
                 let v = Vertex {
                     pos: [i as f32, y as f32, j as f32, 1.0],
                     color: [0.0, 1.0, 0.0, 1.0],
